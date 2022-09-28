ca9c310cd3939bf0a82cbce24164413711e71b70
Akanksha Yadav <programmingwithyadavakanksha.40@gmail.com>
2022-09-28 15:59:00 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const questions = [
+    {
+      id: 1,
+      title: 'Do I have to allow the use of cookies?',
+      info:
+      ' The short answer is, no, you do not have to accept cookies. Rules like the GDPR were designed to give you control over your data and browsing history.'
+    },
+    {
+      id: 2,
+      title: 'How do I change my My Page password?',
+      info:
+      'At the top, tap Security. Under "Signing in to Google," tap Password. You might need to sign in. Enter your new password, then tap Change Password.'
+    },
+    {
+      id: 3,
+      title: 'What is BankID?',
+      info:
+      'BankID is a personal and easy electronic credential for secure identification and signing online.'
+    },
+    {
+      id: 4,
+      title: 'When do I recieve a password ordered by letter?',
+      info:
+      'You need to create a password to do just about everything on the Web, ... of this password would use symbols, uppercase letters, and a more random order'
+    },
+  ]
+  export default questions
+  
